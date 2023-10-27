615bf114835950e38ebe671b1e5d9d30d010defd
r <scot0410@d.umn.edu>
2023-10-26 19:32:27 -0500

    Initial commit

---
+++ src/main/java/com/example/UpsService/UpsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.UpsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpsServiceApplication.class, args);
+	}
+
+}
